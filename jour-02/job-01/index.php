600302178774db74cfe1cde5c323b81778dbf0fa
anais-labit <anais.labit@gmail.com>
2023-09-07 11:00:12 +0200

    ajout tableau HTML

--- jour-02/job-01/index.php
+++ jour-02/job-01/index.php
@@ -16,4 +16,56 @@ function find_all_students(): array
     return $students;
 }
 
-find_all_students();
+$result = find_all_students();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job-01</title>
+</head>
+
+<body>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Liste des étudiants</th>
+            </tr>
+            <tr>
+                <th>id</th>
+                <th>grade_id</th>
+                <th>email</th>
+                <th>fullname</th>
+                <th>birthdate</th>
+                <th>gender</th>
+            </tr>
+        <tbody>
+            <tr>
+                <td>
+                    <?php foreach ($result as $student) : ?>
+            </tr>
+            <tr>
+
+                <td><?php echo $student['id'] ?></td>
+                <td><?php echo $student['grade_id'] ?></td>
+                <td><?php echo $student['email'] ?></td>
+                <td><?php echo $student['fullname'] ?></td>
+                <td><?php echo $student['birthdate'] ?></td>
+                <td><?php echo $student['gender'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </td>
+        </tr>
+        </tbody>
+
+    </table>
+
+</body>
+
+</html>
